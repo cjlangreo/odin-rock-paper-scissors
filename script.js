39714d8df6994a3270c6ba014632cd5f14c9a7a8
Chanz Jryko Langreo <langreo.grim@gmail.com>
2026-01-25 15:41:22 +0800

    getComputerChoice() init

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const choices = ["rock", "paper", "scissors"]
 let humanScore = 0;
 let computerScore = 0;
 const TOTAL_ROUNDS = 5;
+
+function getComputerChoice(){
+  const randomNumber = Math.floor(Math.random() * 3);
+  const choice = choices[randomNumber]
+  console.log(`Computer chose ${choice}!`);
+  return choice
+}
